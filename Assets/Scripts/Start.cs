25b60cdd1d63b30699e3a422d9937e1da9f91b88
Momin Khan <foolmoron@gmail.com>
2017-07-30 21:17:59 -0400

    Title screen

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+
+    float timer;
+    
+    void Awake() {
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer > 2 && Input.anyKeyDown) {
+            Destroy(gameObject);
+        } else if (timer > 10) {
+            Destroy(gameObject);
+        }
+    }
+}
